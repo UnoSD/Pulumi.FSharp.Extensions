67d0533edc528fe81d78f7804aad081cfd3d138d
UnoSD <unosd.dev@gmail.com>
2020-06-25 19:44:41 +0100

    Move commented tags logic

--- Regions.fs
+++ Regions.fs
@@ -121,15 +121,3 @@ module Core =
         | SwitzerlandWest -> "switzerlandwest"
         | UKWest -> "ukwest"
         | UAECentral -> "uaecentral"
-        
-    // Avoid repetition of set tags code on all resources
-    (*type ITags =
-        [<CustomOperation("tags")>]
-        member inline __.Tags(args : ^a when ^a : (member SetTags : ^a -> ^a),
-                              tags) =
-            args.SetTags(tags)
-        
-        member inline __.Tags(args : ^a when ^a : (member SetTags : ^a -> ^a),
-                              tags) =
-            args.SetTags(tags |>
-                         List.map (fun (n, v) -> (n, input v)))*)
---
+++ Tags.fs
@@ -0,0 +1,16 @@
+namespace Pulumi.FSharp.Azure
+
+module Core =
+    
+    ()
+    // Avoid repetition of set tags code on all resources
+    (*type ITags =
+        [<CustomOperation("tags")>]
+        member inline __.Tags(args : ^a when ^a : (member SetTags : ^a -> ^a),
+                              tags) =
+            args.SetTags(tags)
+        
+        member inline __.Tags(args : ^a when ^a : (member SetTags : ^a -> ^a),
+                              tags) =
+            args.SetTags(tags |>
+                         List.map (fun (n, v) -> (n, input v)))*)
