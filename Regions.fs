a2854fe1e34779841e27ab5eed884a38e277360e
UnoSD <unosd.dev@gmail.com>
2020-06-25 15:38:09 +0100

    Add all available Azure regions for a VS subscription

--- Regions.fs
+++ Regions.fs
@@ -1,11 +1,135 @@
 namespace Pulumi.FSharp.Azure
 
 module Core =    
-    // Get list from Azure REST API
+    // Get list from Azure REST API (find anonymous endpoint)
+    // Create type provider to convert to discriminated union the response of:
+    // https://management.azure.com/subscriptions/{subscriptionId}/locations?api-version=2020-01-01
     type Region =
+        | EastUS
+        | EastUS2
+        | SouthCentralUS
+        | WestUS2
+        | AustraliaEast
+        | SoutheastAsia
+        | NorthEurope
+        | UKSouth
         | WestEurope
+        | CentralUS
+        | NorthCentralUS
+        | WestUS
+        | SouthAfricaNorth
+        | CentralIndia
+        | EastAsia
+        | JapanEast
+        | KoreaCentral
+        | CanadaCentral
+        | FranceCentral
+        | GermanyWestCentral
+        | NorwayEast
+        | SwitzerlandNorth
+        | UAENorth
+        | BrazilSouth
+        | CentralUSStage
+        | EastUS2Stage
+        | NorthCentralUSStage
+        | SouthCentralUSStage
+        | WestUSStage
+        | WestUS2Stage
+        | AsiaPacific
+        | Australia
+        | Europe
+        | Global
+        | Japan
+        | UK
+        | UnitedStates
+        | EastAsiaStage
+        | CentralUSEUAP
+        | EastUS2EUAP
+        | WestCentralUS
+        | SouthAfricaWest
+        | AustraliaCentral
+        | AustraliaCentral2
+        | AustraliaSoutheast
+        | JapanWest
+        | KoreaSouth
+        | SouthIndia
+        | WestIndia
+        | CanadaEast
+        | FranceSouth
+        | GermanyNorth
+        | NorwayWest
+        | SwitzerlandWest
+        | UKWest
+        | UAECentral
     
-    // Get names from union type cases formatted instead
+    // Get names from union type cases formatted instead (or provided by the REST API above)
     let regionName =
         function
-        | WestEurope -> "West Europe"
+        | EastUS -> "eastus"
+        | EastUS2 -> "eastus2"
+        | SouthCentralUS -> "southcentralus"
+        | WestUS2 -> "westus2"
+        | AustraliaEast -> "australiaeast"
+        | SoutheastAsia -> "southeastasia"
+        | NorthEurope -> "northeurope"
+        | UKSouth -> "uksouth"
+        | WestEurope -> "westeurope"
+        | CentralUS -> "centralus"
+        | NorthCentralUS -> "northcentralus"
+        | WestUS -> "westus"
+        | SouthAfricaNorth -> "southafricanorth"
+        | CentralIndia -> "centralindia"
+        | EastAsia -> "eastasia"
+        | JapanEast -> "japaneast"
+        | KoreaCentral -> "koreacentral"
+        | CanadaCentral -> "canadacentral"
+        | FranceCentral -> "francecentral"
+        | GermanyWestCentral -> "germanywestcentral"
+        | NorwayEast -> "norwayeast"
+        | SwitzerlandNorth -> "switzerlandnorth"
+        | UAENorth -> "uaenorth"
+        | BrazilSouth -> "brazilsouth"
+        | CentralUSStage -> "centralusstage"
+        | EastUS2Stage -> "eastus2stage"
+        | NorthCentralUSStage -> "northcentralusstage"
+        | SouthCentralUSStage -> "southcentralusstage"
+        | WestUSStage -> "westusstage"
+        | WestUS2Stage -> "westus2stage"
+        | AsiaPacific -> "asiapacific"
+        | Australia -> "australia"
+        | Europe -> "europe"
+        | Global -> "global"
+        | Japan -> "japan"
+        | UK -> "uk"
+        | UnitedStates -> "unitedstates"
+        | EastAsiaStage -> "eastasiastage"
+        | CentralUSEUAP -> "centraluseuap"
+        | EastUS2EUAP -> "eastus2euap"
+        | WestCentralUS -> "westcentralus"
+        | SouthAfricaWest -> "southafricawest"
+        | AustraliaCentral -> "australiacentral"
+        | AustraliaCentral2 -> "australiacentral2"
+        | AustraliaSoutheast -> "australiasoutheast"
+        | JapanWest -> "japanwest"
+        | KoreaSouth -> "koreasouth"
+        | SouthIndia -> "southindia"
+        | WestIndia -> "westindia"
+        | CanadaEast -> "canadaeast"
+        | FranceSouth -> "francesouth"
+        | GermanyNorth -> "germanynorth"
+        | NorwayWest -> "norwaywest"
+        | SwitzerlandWest -> "switzerlandwest"
+        | UKWest -> "ukwest"
+        | UAECentral -> "uaecentral"
+        
+    // Avoid repetition of set tags code on all resources
+    (*type ITags =
+        [<CustomOperation("tags")>]
+        member inline __.Tags(args : ^a when ^a : (member SetTags : ^a -> ^a),
+                              tags) =
+            args.SetTags(tags)
+        
+        member inline __.Tags(args : ^a when ^a : (member SetTags : ^a -> ^a),
+                              tags) =
+            args.SetTags(tags |>
+                         List.map (fun (n, v) -> (n, input v)))*)
