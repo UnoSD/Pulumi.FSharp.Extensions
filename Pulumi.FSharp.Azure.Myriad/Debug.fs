bfe8b06393c9f866b32309445bdec3094b5c82a8
UnoSD <unosd.dev@gmail.com>
2020-07-23 20:15:27 +0200

    More types for debug

--- Pulumi.FSharp.Azure.Myriad/Debug.fs
+++ Pulumi.FSharp.Azure.Myriad/Debug.fs
@@ -8,14 +8,14 @@ let private isDebug = false
 let debugFilterTypes x =
     x |>
     if isDebug then
-        Array.filter (fst >> (function | Type x -> x.ResourceType.Value = "VirtualMachineStorageOsDisk"
-                                       | Resource x -> x.ResourceTypeCamelCase.Value = "virtualMachine"))
+        Array.filter (fst >> (function | Type x -> List.contains x.ResourceType.Value [ "VirtualMachineStorageOsDisk"; "getAccountSASPermissions"; "AccountNetworkRules" ]
+                                       | Resource x -> List.contains x.ResourceTypeCamelCase.Value [ "virtualMachine"; "accountNetworkRules" ]))
     else
         id
 
 let debugFilterProvider (x : (string * 'a) []) : ((string * 'a) []) =
     x |>
     if isDebug then
-        Array.filter (fun (provider, _) -> provider = "compute")
+        Array.filter (fun (provider, _) -> List.contains provider [ "compute"; "storage"; "advisor" ])
     else
         id
