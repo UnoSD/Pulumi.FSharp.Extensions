3154dfabb4058ad80d880232659fd7652d741db1
UnoSD <unosd.dev@gmail.com>
2020-07-24 18:44:17 +0200

    Forgot to upgrade version with the NuGet package

--- Pulumi.FSharp.Azure/Myriad.fs
+++ Pulumi.FSharp.Azure/Myriad.fs
@@ -1,6 +1,6 @@
 module private Azure
 
 let private config = {|
-    Version = "3.11.0" // Version needs to match NuGet package Pulumi.Azure
+    Version = "3.12.1" // Version needs to match NuGet package Pulumi.Azure
     ChangeValueToForceReGeneration = false
 |}
