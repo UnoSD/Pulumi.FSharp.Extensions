3d02039cfe9978e27409bc30884871ff93d424a8
UnoSD <unosd.dev@gmail.com>
2021-02-11 11:25:03 +0000

    Update Aws to 3.28.1 and fix package version for Azure

--- Pulumi.FSharp.Aws/Myriad.fs
+++ Pulumi.FSharp.Aws/Myriad.fs
@@ -1,6 +1,6 @@
 module private Aws
 
 let private config = {|
-    Version = "2.13.1" // Version needs to match NuGet package Pulumi.Aws
+    Version = "3.28.1" // Version needs to match NuGet package Pulumi.Aws
     ChangeValueToForceReGeneration = false
 |}
