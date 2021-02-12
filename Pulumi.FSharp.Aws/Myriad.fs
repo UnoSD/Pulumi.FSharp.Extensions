f7a48b4a584973cdc90c1e6ca76e378e88799911
UnoSD <unosd.dev@gmail.com>
2021-02-12 17:30:56 +0000

    Comment hack as Myriad doesn't like it in DevOps

--- Pulumi.FSharp.Aws/Myriad.fs
+++ Pulumi.FSharp.Aws/Myriad.fs
@@ -4,4 +4,4 @@ let private config = {|
     Version = "3.28.1" // Version needs to match NuGet package Pulumi.Aws
 |}
 
-module Force = let private nonce = 30171
+//module Force = let private nonce = 16490
--- Pulumi.FSharp.Azure/Myriad.fs
+++ Pulumi.FSharp.Azure/Myriad.fs
@@ -4,4 +4,4 @@ let private config = {|
     Version = "3.46.0" // Version needs to match NuGet package Pulumi.Azure
 |}
 
-module Force = let private nonce = 30171
+//module Force = let private nonce = 965
