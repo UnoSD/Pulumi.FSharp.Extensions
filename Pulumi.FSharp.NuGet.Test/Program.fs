c906110ffb18551c6d979850d0cdf0fe62cf1597
UnoSD <unosd.dev@gmail.com>
2021-02-20 10:53:50 +0000

    Improve example/test

--- Pulumi.FSharp.NuGet.Test/Program.fs
+++ Pulumi.FSharp.NuGet.Test/Program.fs
@@ -4,9 +4,12 @@ open Pulumi.FSharp.Kubernetes.Apps.V1.Inputs
 open Pulumi.FSharp.Kubernetes.Core.V1.Inputs
 open Pulumi.FSharp.Kubernetes.Meta.V1.Inputs
 open Pulumi.FSharp.Azure.Compute.Inputs
+open Pulumi.FSharp.Azure.Network.Inputs
 open Pulumi.FSharp.Azure.Compute
+open Pulumi.FSharp.Azure.Network
 open Pulumi.FSharp.Azure.Storage
 open Pulumi.FSharp.Aws.S3.Inputs
+open Pulumi.FSharp.Azure.Core
 open Pulumi.FSharp.AzureAD
 open Pulumi.FSharp.Assets
 open Pulumi.FSharp.Aws.S3
@@ -16,6 +19,7 @@ open Pulumi.FSharp
 
 let deployment = Kubernetes.Apps.V1.deployment
 let container = Kubernetes.Core.V1.Inputs.container
+let storageContainer = Azure.Storage.container
 
 (*
 Backup
@@ -25,56 +29,26 @@ $ echo -n "Aws Azure AzureAD Kubernetes" | xargs -I{} -n1 -d' ' bash -c 'diff -q
 *)
 
 let infra () =
-    
     let _ =
-        blob {
-            name "storageblob1"
-            storageContainerName "cont"
-            storageAccountName "storage"
-            resourceType "Block"
-            source { Path = "Program.fs" }.ToPulumiType
-        }
-        
-    let _ =
-        blob {
-            name "storageblob2"
-            storageContainerName "cont"
-            storageAccountName "storage"
-            resourceType "Block"
-            source { Uri = "https://raw.githubusercontent.com/UnoSD/Pulumi.FSharp.Extensions/master/README.md" }.ToPulumiType
-        }
-        
-    let _ =
-        blob {
-            name "storageblob3"
-            storageContainerName "cont"
-            storageAccountName "storage"
-            resourceType "Block"
-            source { Assets = Map.empty
-                                 .Add("name" , File { Path = "Program.fs" })
-                                 .Add("name2", String { Text = "text" }) }.ToPulumiType
-        }
-    
-    let application =
         deployment {
-            name "application"            
+            name "application"
 
             deploymentSpec {
                 replicas 1
-                
-                labelSelector {
+
+                labelSelector { 
                     matchLabels [ "app", input "nginx" ]
                 }
 
-                podTemplateSpec {                    
-                    objectMeta { 
+                podTemplateSpec {
+                    objectMeta {
                         labels [ "app", input "nginx" ]
                     }
 
                     podSpec {
-                        containers [                            
+                        containers [
                             container {
-                                name "nginx"
+                                name  "nginx"
                                 image "nginx"
                                 ports [ containerPort { containerPortValue 80 } ]
                             }
@@ -84,72 +58,147 @@ let infra () =
             }
         }
 
-    let k8sAppName =
-        output {
-            let! md = application.Metadata
-            
-            return md.Name
-        }
-
-    let bucket =
+    let _ =
         bucket {
-            name "my-bucket"
-            
-            bucketWebsite {
+            name "bucket-example"
+
+            bucketWebsite { 
                 indexDocument "index.html"
             }
         }
-        
-    let aadGroup =
+
+    let _ =
         group {
-            name "aad-app"
-            displayName "Test AAD group"
+            name        "group-example"
+            displayName "Example AAD group from Pulumi.FSharp.Extensions"
         }
-    
+
+    let rg =
+        resourceGroup {
+            name     "rg-example"
+            location "West Europe"
+        }
+
+    let storage =
+        account {
+            resourceGroup          rg.Name
+            location               rg.Location
+            name                   "stexample"
+            accountTier            "Standard"
+            accountReplicationType "LRS"
+        }
+
+    let container =
+        storageContainer { 
+            storageAccountName  storage.Name
+            name                "example"
+            containerAccessType "private"
+        }
+
+    let _ =
+        blob {
+            name                 "file-blob"
+            resourceType         "Block"
+            storageContainerName container.Name
+            storageAccountName   storage.Name
+            source               { Path = "Program.fs" }.ToPulumiType
+        }
+
+    let _ =
+        blob {
+            name                 "url-blob"
+            resourceType         "Block"
+            storageContainerName container.Name
+            storageAccountName   storage.Name
+
+            source {
+                Uri = "https://raw.githubusercontent.com/UnoSD/Pulumi.FSharp.Extensions/master/README.md"
+            }.ToPulumiType
+        }
+
+    let _ =
+        blob {
+            name                 "archive-blob"
+            resourceType         "Block"
+            storageContainerName container.Name
+            storageAccountName   storage.Name
+
+            source { 
+                Assets = Map.empty
+                            .Add("pr.fs", File   { Path = "Program.fs" })
+                            .Add("p.txt", String { Text = "text!!!!!!" }) 
+            }.ToPulumiType
+        }
+
+    let vnet =
+        virtualNetwork {
+            name          "vnet-example"
+            addressSpaces "10.0.0.0/16"
+            location      rg.Location
+            resourceGroup rg.Name
+        }
+
+    let subnet =
+        subnet {
+            name               "internal"
+            resourceGroup      rg.Name
+            virtualNetworkName vnet.Name
+            addressPrefixes    "10.0.2.0/24"
+        }
+
+    let nic =
+        networkInterface {
+            name          "nic-example"
+            location      rg.Location
+            resourceGroup rg.Name
+
+            ipConfigurations [ 
+                networkInterfaceIpConfiguration {
+                    name                       "internal"
+                    subnetId                   subnet.Id
+                    privateIpAddressAllocation "Dynamic"
+                }
+            ]
+        }
+
     let vm =
         windowsVirtualMachine {
-            name "development"
-            resourceName "development"
-            
-            resourceGroup "Development"
-            networkInterfaceIds [ config.["vmNicId"] ]
-            size "Standard_D4s_v3"
-            
+            name                "vm-example"
+            resourceName        "vm-example"
+            size                "Standard_A1_v2"
+            location            rg.Location
+            resourceGroup       rg.Name
+            networkInterfaceIds nic.Id            
+
             windowsVirtualMachineOsDisk {
-                name config.["vmDiskName"]
-                caching "ReadWrite"
+                name               "osdiskexample"
+                caching            "ReadWrite"
                 storageAccountType "Standard_LRS"
             }
-            
+
             adminUsername config.["vmUser"]
             adminPassword secret.["vmPass"]
-            
+
             windowsVirtualMachineSourceImageReference {
-                offer "visualstudio2019latest"
-                publisher "microsoftvisualstudio"
-                sku "vs-2019-comm-latest-win10-n"
-                version "latest"
+                offer     "WindowsServer"
+                publisher "MicrosoftWindowsServer"
+                sku       "2016-Datacenter"
+                version   "latest"
             }
         }
 
     let secretValue =
-        secretOutput {
-            return vm.PrivateIpAddress
-        }
+        secretOutput { return vm.PrivateIpAddress }
 
     let pipCird =
         output {
             let! pip = vm.PrivateIpAddress
             
-            return pip + "/32"
+            return $"{pip}/32"
         }
 
-    dict [ "SecretPrivateIP",      secretValue            :> obj
-           "VisiblePrivateIPCIDR", pipCird                :> obj
-           "K8sAppName",           k8sAppName             :> obj
-           "AwsBucket",            bucket.WebsiteEndpoint :> obj
-           "AadGroup",             aadGroup.ObjectId      :> obj ]
+    dict [ "SecretPrivateIP"     , secretValue            :> obj
+           "VisiblePrivateIPCIDR", pipCird                :> obj ]
 
 [<EntryPoint>]
-let main _ =
-  Deployment.run infra
+let main _ = Deployment.run infra
