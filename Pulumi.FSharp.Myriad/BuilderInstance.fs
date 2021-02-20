2dee281aeca8a94ed4e4d34a475e3c232f0e7f8b
UnoSD <unosd.dev@gmail.com>
2021-02-20 11:48:29 +0000

    Remove unsupported list from XML documentation

--- Pulumi.FSharp.Myriad/BuilderInstance.fs
+++ Pulumi.FSharp.Myriad/BuilderInstance.fs
@@ -21,19 +21,16 @@ let createBuilderInstance description typeName pTypes =
     let builderNamesSection =
         match builderNames |> List.ofArray with
         | [] -> [ "</summary>" ]
-        | bn -> let fn = List.map (fun x -> $"<item><c>{x}</c></item>") bn
-                "<list type=\"bullet\">" :: "<strong>Nested properties</strong>" :: fn @ [ "</list></summary>" ]
+        | bn -> let fn = List.map (fun x -> $"<c>{x}</c>") bn
+                "<strong>Nested computational expressions</strong>" :: fn @ [ "</summary>" ]
     
     let descriptionShort =
         description |> String.split '\n' |> Array.head
     
     seq {
         yield $"<summary>{descriptionShort}"
-        yield  "*** Available properties ***"
-        yield  ""
         yield  "<strong>Operations</strong>"
-        yield  "<list type=\"bullet\">"
-        yield! ops |> Array.map (fun x -> $"<item><c>{x.OperationName}</c></item>") |> Array.append [| "</list>" |]
+        yield! ops |> Array.map (fun x -> $"<c>{x.OperationName}</c>")
         yield! builderNamesSection } |>
     createLet (toCamelCase typeName)
               (createInstance $"{typeName}Builder" SynExpr.CreateUnit)
