3ddce2c4353377bcc9ac016538c09ff1f1882dfe
UnoSD <unosd.dev@gmail.com>
2021-02-21 13:09:12 +0000

    Remove FAKE vault and use NuGet version

--- build.fsx
+++ build.fsx
@@ -7,10 +7,10 @@ nuget Fake.Core.Xml
 nuget Fake.Core.Target //"
 #load ".fake/build.fsx/intellisense.fsx"
 open Fake.IO.Globbing.Operators
+open Fake.DotNet.NuGet
 open Fake.BuildServer
 open Fake.Core.Xml
 open Fake.DotNet
-open System.IO
 open Fake.Core
 open System
 
@@ -19,20 +19,11 @@ open System
 
 BuildServer.install [ TeamFoundation.Installer ]
 
-let vaultFile =
-    FileInfo("Pulumi.FSharp.Extensions.vault.json")
-
-let vault = 
-    match BuildServer.buildServer, vaultFile.Exists with
-    | TeamFoundation, _ -> TeamFoundation.variables
-    | _, true           -> vaultFile.OpenText().ReadToEnd() |> Vault.fromJson
-    | _, false          -> failwith "Unsupported source for secrets"
-
 let provider =
-    match BuildServer.buildServer with
-    | TeamFoundation -> Environment.environVarOrFail "PROVIDER"
-    | LocalBuild     -> Context.forceFakeContext().Arguments |> List.exactlyOne
-    | _              -> failwith "Missing provider or unsupported build server"
+    match Environment.environVarOrNone "PROVIDER", lazy(Context.forceFakeContext().Arguments) with
+    | Some p, _
+    | None  , Lazy([ p ]) -> p
+    | _                   -> failwith "Missing provider"
 
 let fullName =
     sprintf "Pulumi.FSharp.%s" provider
@@ -62,19 +53,19 @@ let buildOptions options : DotNet.BuildOptions = {
             NoLogo = true
     }
 
-let extensionsVersion =
-    match BuildServer.buildServer, lazy(Int32.TryParse(BuildServer.buildVersion)) with
-    | LocalBuild, _                   -> ""
-    | _         , Lazy(true, version) -> sprintf ".%i" version
-    | _                               -> "Unsupported extension version, if using Azure DevOps try and set " +
-                                         "`name: $(Rev:r)` at the top of the pipeline"
+let nextExtensionsVersion =
+    NuGet.getLatestPackage (NuGet.getRepoUrl()) fullName |>
+    (fun x -> x.Version.Split('.')) |>
+    Array.last |>
+    Int32.Parse |>
+    (+)1
 
 let packOptions options : DotNet.PackOptions = {
     options with
         MSBuildParams = { 
             options.MSBuildParams with
                 Properties = 
-                    ("PackageVersion", sprintf "%s%s" pulumiNuGetVersion extensionsVersion) :: 
+                    ("PackageVersion", sprintf "%s.%i" pulumiNuGetVersion nextExtensionsVersion) :: 
                     options.MSBuildParams.Properties
         }
 }
@@ -83,7 +74,7 @@ let pushOptions options : DotNet.NuGetPushOptions = {
     options with
         PushParams = {
             options.PushParams with
-                ApiKey = Vault.tryGet "apiKey" vault
+                ApiKey = Environment.environVarOrFail "NUGETAPIKEY" |> Some
                 Source = Some "https://api.nuget.org/v3/index.json"
         }
 }
