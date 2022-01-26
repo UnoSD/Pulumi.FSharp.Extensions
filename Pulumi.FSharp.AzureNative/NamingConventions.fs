68fdb56f0cad3ec11bbf6f71d9bf5f571fa9b2eb
UnoSD <1582987+UnoSD@users.noreply.github.com>
2022-01-26 23:41:08 +0100

    Use hardcoded location map

--- Pulumi.FSharp.AzureNative/NamingConventions.fs
+++ Pulumi.FSharp.AzureNative/NamingConventions.fs
@@ -1,17 +1,11 @@
 module Pulumi.FSharp.NamingConventions.Azure
 
-open FSharp.Data
+open AzureLocations
 open Pulumi
 
 // https://docs.microsoft.com/en-us/azure/cloud-adoption-framework/ready/azure-best-practices/resource-naming
 
-module Region = 
-    // az account list-locations -o json | sed "s/$(az account show --query id -o tsv)/00000000-0000-0000-0000-000000000000/"
-    type private LocationsProvider =
-        JsonProvider<LocationsJson.jsonData>
-    
-    let private locationsJson = LocationsProvider.GetSamples()
-
+module Region =
     let private shorten (regionName : string) =
         regionName.Replace("europe"      , "eu" )
                   .Replace("brazil"      , "br" )
@@ -25,29 +19,23 @@ module Region =
                   .Replace("east"        , "e"  )
                   .Replace("west"        , "w"  )
     
-    let private configuredRegion = Config("azure-native").Require("location").ToLowerInvariant()
-    
-    let private locationsMap = 
-        locationsJson
-        |> Array.collect(fun lj -> [| lj.RegionalDisplayName.ToLowerInvariant(), lj
-                                      lj.Name.ToLowerInvariant()               , lj
-                                      lj.DisplayName.ToLowerInvariant()        , lj |])
-        |> Map.ofArray
+    let private configuredRegion = Config("azure-native").Get("location")
 
     let shortName =
         locationsMap
         |> Map.tryFind configuredRegion
-        |> Option.map(fun x -> x.Name |> shorten)
-        |> Option.defaultValue (failwith "Missing or incorrect azure-native:location config value")
+        |> Option.map(fun x -> x |> shorten)
+        |> Option.defaultWith (fun () -> failwith $"Missing or incorrect azure-native:location: {configuredRegion}")
 
 module Resource =
-    open Pulumi.FSharp.Config
-    
     let private woa =
-        config.["workloadOrApplication"]
+        match Config().Get("workloadOrApplication") with
+        | null
+        | ""    -> ""
+        | value -> $"{value}-"
     
     let name resourceType (instanceNumber : int) =
-        $"{resourceType}-{woa}-{Deployment.Instance.StackName}-{Region.shortName}-{instanceNumber:D3}"
+        $"{resourceType}-{woa}{Deployment.Instance.StackName}-{Region.shortName}-{instanceNumber:D3}"
         
     let nameOne resourceType =
         name resourceType 1
