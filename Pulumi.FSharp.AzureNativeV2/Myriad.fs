ce8ef09d6aae02934ec8be387ec66826e6486147
Mikhail Smal <mikhail@smal.dev>
2023-11-03 22:13:18 +0100

    AzureNative V2

---
+++ Pulumi.FSharp.AzureNativeV2/Myriad.fs
@@ -0,0 +1,3 @@
+module private AzureNativeV2
+
+module Force = let private nonce = 1827270720
--- Pulumi.FSharp.Myriad/Generator.fs
+++ Pulumi.FSharp.Myriad/Generator.fs
@@ -11,7 +11,7 @@ open Schema
 [<MyriadGenerator("Pulumi.FSharp")>]
 type PulumiFSharpGenerator() =
     interface IMyriadGenerator with
-        member _.Generate(context) =            
+        member _.Generate(context) =
             let projectFile =
                 FileInfo(context.InputFilename).Directory.EnumerateFiles("*.fsproj") |>
                 Seq.exactlyOne
@@ -19,10 +19,19 @@ type PulumiFSharpGenerator() =
             let provider =
                 projectFile.Name["Pulumi.FSharp.".Length..^".fsproj".Length]
 
+            let providerNameOverride =
+                Map.empty
+                    .Add("AzureNativeV2", "AzureNative")
+
+            let provider =
+                providerNameOverride |>
+                Map.tryFind provider |>
+                Option.defaultValue provider
+
             let fsproj =
                 XmlDocument() in fsproj.Load (projectFile.OpenText())
-                
-            let version = 
+
+            let version =
                 fsproj.SelectNodes("/Project/ItemGroup") |>
                 Seq.cast<XmlNode> |>
                 Seq.collect (fun y -> y.SelectNodes("PackageReference") |> Seq.cast<XmlNode>) |>
@@ -34,17 +43,17 @@ type PulumiFSharpGenerator() =
             let providerRepositoryNameOverride =
                 Map.empty
                    .Add("AzureNative", "azure-native")
-            
+
             let providerRepository =
                 providerRepositoryNameOverride |>
                 Map.tryFind provider |>
                 Option.defaultValue provider
-            
+
             loadSchema providerRepository version |>
             createTypes |>
             createModules provider |>
             createNamespace |>
-            List.singleton |>           
+            List.singleton |>
             Output.Ast
 
         member this.ValidInputExtensions = seq { ".fs" }
